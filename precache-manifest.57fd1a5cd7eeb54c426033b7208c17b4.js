cffc004fc60d8c771fd699b90f8bec1ad6c1e665
xuwangdong <corey@192.168.0.104>
2021-05-03 00:02:03 +0800

    build

---
+++ precache-manifest.57fd1a5cd7eeb54c426033b7208c17b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0b6d625243c09fbb56d488d03e72aa",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "77b69a95f0363e3e77a7",
+    "url": "/admin/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "1d6ac5356bde1022517f",
+    "url": "/admin/static/css/main.4ead73db.chunk.css"
+  },
+  {
+    "revision": "77b69a95f0363e3e77a7",
+    "url": "/admin/static/js/2.d6b30182.chunk.js"
+  },
+  {
+    "revision": "1d6ac5356bde1022517f",
+    "url": "/admin/static/js/main.07c4c0c9.chunk.js"
+  },
+  {
+    "revision": "d28d0a4b84fb813e6862",
+    "url": "/admin/static/js/runtime~main.d1c93fd9.js"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/admin/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/admin/static/media/logo.ba1f87ec.png"
+  }
+]);
